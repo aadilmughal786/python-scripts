a70af3e9e15a3002c7c516556b7a9598f621c90c
Aadil Mugal <aadilshe786@gmail.com>
2021-07-31 16:28:14 +0530

    add a function for cpu usage

--- system.py
+++ system.py
@@ -5,3 +5,8 @@ def check_disk_usage(disk):
 	du = shutil.disk_usage(disk)
 	free = du.free/du.total*100
 	return free>20
+
+def check_cpu_usage():
+	usage = psutil.cpu_percent(1)
+	return usage<75
+
