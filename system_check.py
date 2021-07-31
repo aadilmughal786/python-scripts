f0bbfdb484c84a5d34cf55a417f4d1791ef402cd
Aadil Mugal <aadilshe786@gmail.com>
2021-07-31 16:35:58 +0530

    add a condation in script

---
+++ system_check.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import shutil
+import psutil
+
+def check_disk_usage(disk):
+	du = shutil.disk_usage(disk)
+	free = du.free/du.total*100
+	return free>20
+
+def check_cpu_usage():
+	usage = psutil.cpu_percent(1)
+	return usage<75
+
+if check_cpu_usage() and check_disk_usage('/'):
+	print("Everything is OK!!")
+else:
+	print("ERROR!")
